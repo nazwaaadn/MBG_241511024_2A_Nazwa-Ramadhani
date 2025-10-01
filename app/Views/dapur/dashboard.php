7d29ac9d9e3fe488e30afd9f03c87aac72d21edb
nazwaaadn <nazwaramadhaninenden@gmail.com>
2025-10-01 16:01:08 +0700

    Add Fitur Lihat Status Permintaan di akun gudang by Nazwa Ramadhani

--- app/Views/dapur/dashboard.php
+++ app/Views/dapur/dashboard.php
@@ -6,11 +6,9 @@
       <div class="item item-1">
         <div class="img-fill">
           <div class="text-content">
-            <h4>Course Registration<br>Made Simple</h4>
+            <h4>Tambah data dapur<br>Made Simple</h4>
             <p>
-              This application helps students register for courses online with ease.
-              A secure and efficient system that allows you to select, manage,
-              and track your course schedule provided by the university.
+              Aplikasi ini memudahkan dapur untuk melakukan proses permintaan data dapur
             </p>
           </div>
         </div>
@@ -27,13 +25,13 @@
         </div>
 
         <div class="col-md-5">
-          <a href="<?= base_url('/addnewcourse') ?>">
+          <a href="<?= base_url('/addPermintaan') ?>">
             <div class="service-item justify-content-center">
               <img src="Student/assets/images/service_01.jpg" alt="">
               <div class="down-content">
                 <center><img src="<?= base_url('image/inbox.png') ?>" alt="" srcset="" style="width: 50px; height: 50px; margin-bottom: 10px;"></center>
-                <h4 class="mt-2">Add Course</h4>
-                <p>Select the courses you want to enroll in. Make sure they fit your schedule and interests for the best learning experience.</p>
+                <h4 class="mt-2">Add Permintaan</h4>
+                <p>Tambah permintaan</p>
 
               </div>
             </div>
@@ -47,8 +45,8 @@
               <img src="Student/assets/images/service_02.jpg" alt="">
               <div class="down-content">
                 <center><img src="<?= base_url('image/learning.png') ?>" alt="" srcset="" style="width: 50px; height: 50px; margin-bottom: 10px;"></center>
-                <h4 class="mt-2">My Courses</h4>
-                <p>Here’s a list of the courses you’re currently enrolled in. Keep track of them to stay on top of your learning.</p>
+                <h4 class="mt-2">Lihat Permintaan</h4>
+                <p>Lihat Permintaan anda</p>
                 <br>
               </div>
             </div>
@@ -64,48 +62,27 @@
       <div class="container">
         <div class="single-services mb-5">
           <div class="container">
-            <div class="row">
-              <div class="col-lg-12 mb-5 align-items-center">
-                <center>
-                  <h3>My Courses</h3>
-                  <p>Berikut adalah daftar mata kuliah yang telah Anda ambil:</p>
-                </center>
-              </div>
-              <div class="col-md-3">
-                <div class="list-group" id="v-pills-tab" role="tablist">
-                  <a class="list-group-item list-group-item-action active"
-                    id="theory-tab"
-                    data-bs-toggle="pill"
-                    href="#theory"
-                    role="tab"
-                    aria-controls="theory"
-                    aria-selected="true">
-                    Mata Kuliah Teori <i class="fa fa-angle-right float-end"></i>
-                  </a>
-                  <a class="list-group-item list-group-item-action"
-                    id="practice-tab"
-                    data-bs-toggle="pill"
-                    href="#practice"
-                    role="tab"
-                    aria-controls="practice"
-                    aria-selected="false">
-                    Mata Kuliah Praktek <i class="fa fa-angle-right float-end"></i>
-                  </a>
-                  <a class="list-group-item list-group-item-action"
-                    id="other-tab"
-                    data-bs-toggle="pill"
-                    href="#other"
-                    role="tab"
-                    aria-controls="other"
-                    aria-selected="false">
-                    Lainnya <i class="fa fa-angle-right float-end"></i>
-                  </a>
-                </div>
-              </div>
+            <table class="table caption-top">
+        <thead>
+            <tr>
+                <th scope="col">tanggal masak</th>
+                <th scope="col">jumlah porsi</th>
+                <th scope="col">menu makan</th>
+                <th scope="col">status</th>
+            </tr>
+        </thead>
+        <tbody id="result">
+            <?php foreach ($BahanBaku as $bb): ?>
+                <tr>
+                    <td><?= $bb['tanggal_masak']; ?></td>
+                    <td><?= $bb['jumlah_porsi']; ?></td>
+                    <td><?= $bb['menu_makan']; ?></td>
+                    <td><?= $bb['status']; ?></td>
 
-
-              
-            </div>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
           </div>
         </div>
       </div>
@@ -113,46 +90,4 @@
     </div>
   </section>
 
-  <!-- <script>
-    const user = [
-        {
-            user_id: 2717,
-            username: "nazwa"
-        }
-    ];
-
-    const user_student = user.filter(u => u.user_id.startsWith("STD"));
-
-    function render(userArray, containerId) {
-        const container = document.getElementById(containerId);
-        userArray.forEach(user => {
-            const card = document.createElement("div");
-            card.className = "";
-            card.innerHTML = `<h4>${user.user_id}</h4>`;
-            container.appendChild(card);
-            
-        })
-    }
-    render(user_student, "student-contaner");
-  </script>
-
-  <script>
-    const form = document.getElemetById("course-form");
-    const errorMessage = document.getElementById("error-message");
-
-    let tampiljumlah = documet.getElementById("total_student");
-    if (!tampiljumlah) {
-        tampiljumlah = document.createElement("p");
-        tampiljumlah.ig = "total_student";
-        tampiljumlah.className = "";
-        form.prepend(tampiljumlah);
-    }
-    
-    let takes = []; //define array
-
-    function name(params) {
-        setTimeout(() => {
-            
-        }, timeout);
-    }
-  </script> -->
+  
